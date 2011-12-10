6921e81f5a642b7fa4545c32702f54ad170c0e82
Bryan Matsuo <bryan.matsuo@gmail.com>
2011-12-09 16:41:07 -0800

    Add tests for the internal tTest function.
    Checks the evaluation of tests, when
    
        - Test method returns nil
        - Test method returns an error
        - Test method returns a fatal error
        - A runtime panic is uncaught within the Test method.

--- table/test_test.go
+++ table/test_test.go
@@ -7,11 +7,60 @@ package table
  */
 
 import (
-    "testing"
+	"testing"
+	"strings"
+	"os"
 )
 
-
-func TestTest(T *testing.T) {
-
+// Test the internal tTest function.
+type tTestTest struct {
+	fn    func() os.Error
+	fatal bool
+	esubs []string
 }
 
+func (test tTestTest) Test() os.Error { return test.fn() }
+
+var tTestTests = []tTestTest{
+	{func() os.Error { return nil }, false, nil},
+	{func() os.Error { return os.NewError("emsg") }, false, []string{"emsg"}},
+	{func() os.Error { return Fatal("fmsg") }, true, []string{"fmsg"}},
+	{func() os.Error { panic("pmsg") }, true, []string{"pmsg"}},
+}
+
+func TestTTest(t *testing.T) {
+	for i, test := range tTestTests {
+		switch err := tTest(test); {
+		case err == nil && (test.esubs != nil || test.fatal):
+			if test.fatal {
+				t.Errorf("tTestTest %d: unexpected nil error (not fatal %v)", i, test.esubs)
+			} else {
+				t.Errorf("tTestTest %d: unexpected nil error (not %v)", i, test.esubs)
+			}
+		case err == nil:
+			break
+		case test.esubs == nil && !test.fatal:
+			t.Errorf("tTestTest %d: unexpected error %v", i, err)
+		default:
+			var fatal bool
+			switch err.(type) {
+			case FatalError:
+				fatal = true
+			default:
+			}
+			if test.fatal != fatal {
+				if fatal {
+					t.Errorf("tTestTest %d: unexpected fatal error %v", err)
+				} else {
+					t.Errorf("tTestTest %d: unexpected non-fatal error %v", err)
+				}
+			}
+			for j, sub := range test.esubs {
+				estr := err.String()
+				if strings.Index(estr, sub) < 0 {
+					t.Errorf("tTestTest %d: error missing substring %d; %s", i, j, estr)
+				}
+			}
+		}
+	}
+}
