85d8510159f30a41307d9b1407ea54ae70915064
Bryan Matsuo <bryan.matsuo@gmail.com>
2011-12-09 22:51:46 -0800

    Add tests for error functions (their string values).

--- table/errors_test.go
+++ table/errors_test.go
@@ -7,11 +7,50 @@ package table
  */
 
 import (
-    "testing"
+	"testing"
+	"reflect"
+	"regexp"
+	"os"
 )
 
-
-func TestErrors(T *testing.T) {
-
+type errorStringTest struct {
+	err     os.Error
+	matches []string
 }
 
+func (test errorStringTest) Test() os.Error {
+	for i, r := range test.matches {
+		if !regexp.MustCompile(r).MatchString(test.err.String()) {
+			return Errorf("pattern %d (%s) doesn't match error string: %v", i, r, test.err)
+		}
+	}
+	return nil
+}
+
+// This table test is a little wonky because I set the Verbose global option for half of the tests.
+func TestErrorString(t *testing.T) {
+	var errorStringTests = []errorStringTest{
+		{os.NewError(errorstr("hello", Errorf("world"))), []string{`hello: world`}},
+		{os.NewError(errorstrf("hello", "%q", "world")), []string{`hello: "world"`}},
+		{os.NewError(fatalstr("hello", Errorf("world"))), []string{`hello: world`}},
+		{os.NewError(fatalstrf("hello", "%q", "world")), []string{`hello: "world"`}},
+		{Errorf("this is a %s error %v", "formatted", reflect.TypeOf("abc")), []string{"this is a formatted error string"}},
+		{Fatalf("this is a %s error string", "formatted"), []string{"this is a formatted error string"}},
+	}
+	Verbose = true
+	defer func() { Verbose = false }()
+	errorStringTests = append(errorStringTests, []errorStringTest{
+		{os.NewError(errorstr("hello", Errorf("world"))), []string{`hello error: world`}},
+		{os.NewError(errorstrf("hello", "%q", "world")), []string{`hello error: "world"`}},
+		{os.NewError(fatalstr("hello", Errorf("world"))), []string{`hello fatal error: world`}},
+		{os.NewError(fatalstrf("hello", "%q", "world")), []string{`hello fatal error: "world"`}},
+		{Errorf("this is a %s error %v", "formatted", reflect.TypeOf("abc")), []string{"this is a formatted error string"}},
+		{Fatalf("this is a %s error string", "formatted"), []string{"this is a formatted error string"}},
+	}...)
+
+	for i, test := range errorStringTests {
+		if err := tTest(test); err != nil {
+			t.Errorf("errorStringTest %d: %v", i, err)
+		}
+	}
+}
