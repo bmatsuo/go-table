504d7d2e48e8465b60c990ae16c02aae3848e6e2
Bryan Matsuo <bryan.matsuo@gmail.com>
2011-12-11 00:03:04 -0800

    Fix an error logging bug where the name got printed twice.
    Was a problem with formatted logging. It would call the Log/Error/Fatal
    helper and that would cause a double injection of the name.

--- table/testing.go
+++ table/testing.go
@@ -20,27 +20,45 @@ type testingT struct {
 }
 
 func newTestingT(name string, t Testing) *testingT { return &testingT{name, t} }
-func (t *testingT) dup() (cp *testingT)            { cp = new(testingT); *cp = *t; return cp }
-func (t *testingT) sub(name string) (s *testingT)  { s = t.dup(); s.name = t.msg(name); return s }
+func (t *testingT) dup() (cp *testingT)            { cp = new(testingT); *cp = *t; return }
+func (t *testingT) sub(name string) (s *testingT)  { s = newTestingT(name, t); return }
 
-func (t *testingT) msg(v ...interface{}) string            { return msg(t.name, sprint(v...)) }
-func (t *testingT) msgf(f string, v ...interface{}) string { return msg(t.name, sprintf(f, v...)) }
-func (t *testingT) errmsg(typ string, v ...interface{}) string {
-	if Verbose {
-		return msg(sprintf("%s %s", t.name, typ), sprint(v...))
+func (t *testingT) msg(v ...interface{}) string            {
+	m := sprint(v...)
+	if t.name != "" {
+		m = msg(t.name, m)
 	}
-	return msg(t.name, sprint(v...))
+	return m
 }
+func (t *testingT) errmsg(typ string, v ...interface{}) string {
+	name, m := t.name, sprint(v...)
+	if Verbose {
+		prefix := typ
+		if name != "" {
+			prefix = sprintf("%s %s", t.name, typ)
+		}
+		name = msg(name, prefix)
+	}
+	if name != "" {
+		m = msg(name, m)
+	}
+	return m
+}
+func (t *testingT) msgf(f string, v ...interface{}) string { return msg(t.name, sprintf(f, v...)) }
+
 
 func (t *testingT) Fail()                                     { t.t.Fail() }
 func (t *testingT) FailNow()                                  { t.t.FailNow() }
 func (t *testingT) Failed() bool                              { return t.t.Failed() }
-func (t *testingT) Log(args ...interface{})                   { t.t.Log(t.msg(args...)) }
-func (t *testingT) Error(args ...interface{})                 { t.t.Error(t.errmsg("error", args...)) }
-func (t *testingT) Fatal(args ...interface{})                 { t.t.Fatal(t.errmsg("fatal", args...)) }
-func (t *testingT) Logf(format string, args ...interface{})   { t.Log(t.msgf(format, args...)) }
-func (t *testingT) Errorf(format string, args ...interface{}) { t.Error(t.msgf(format, args...)) }
-func (t *testingT) Fatalf(format string, args ...interface{}) { t.Fatal(t.msgf(format, args...)) }
+func (t *testingT) log(args ...interface{})                   { t.t.Log(sprint(args...)) }
+func (t *testingT) error(args ...interface{})                 { t.t.Error(sprint(args...)) }
+func (t *testingT) fatal(args ...interface{})                 { t.t.Fatal(sprint(args...)) }
+func (t *testingT) Log(args ...interface{})                   { t.log(t.msg(args...)) }
+func (t *testingT) Error(args ...interface{})                 { t.error(t.errmsg("error", args...)) }
+func (t *testingT) Fatal(args ...interface{})                 { t.fatal(t.errmsg("fatal", args...)) }
+func (t *testingT) Logf(format string, args ...interface{})   { t.log(t.msgf(format, args...)) }
+func (t *testingT) Errorf(format string, args ...interface{}) { t.error(t.msgf(format, args...)) }
+func (t *testingT) Fatalf(format string, args ...interface{}) { t.fatal(t.msgf(format, args...)) }
 
 type Testing interface {
 	Error(args ...interface{})
