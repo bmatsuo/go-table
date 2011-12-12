b168dc0f178d3038b5c5bd5d9282ebb8d1b9857f
Bryan Matsuo <bryan.matsuo@gmail.com>
2011-12-11 23:37:14 -0800

    Remove extra comment stuff from errors.go because its so simple now.

--- table/errors.go
+++ table/errors.go
@@ -11,9 +11,9 @@ package table
  */
 
 import (
-	"strings"
 	"errors"
 	"fmt"
+	"strings"
 )
 
 var (
@@ -21,21 +21,11 @@ var (
 	MsgFmt  = "%s: %s" // Result output format. Can be changed to another 2-argument string.
 )
 
-/****************************/
-/* General helper functions */
-/****************************/
-
 // Aliases to "fmt" functions.
 func sprint(v ...interface{}) string                 { return fmt.Sprint(v...) }
 func sprintf(format string, v ...interface{}) string { return fmt.Sprintf(format, v...) }
 func errorf(format string, v ...interface{}) error   { return fmt.Errorf(format, v...) }
-
-// Create a new error using the string representation of v.
-func error_(v ...interface{}) error { return errors.New(sprint(v...)) }
-
-/*********************************************/
-/* Helper functions for the Test and friends */
-/*********************************************/
+func error_(v ...interface{}) error                  { return errors.New(sprint(v...)) }
 
 // Create a named messages with name format MsgFmt.
 func msg(name string, v ...interface{}) string {
